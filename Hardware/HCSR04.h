2792b7a52fbcff77e74d40007987f501c6cbc63f
记性差的笨蛋 <164169060+nccyy@users.noreply.github.com>
2025-04-27 14:50:20 +0800

    Add files via upload

---
+++ Hardware/HCSR04.h
@@ -0,0 +1,6 @@
+#ifndef __HCSR04_H
+#define __HCSR04_H
+void HCSR04_Init(void);
+float HCSR04_Distance(void);
+void AvoidObstacle(void);
+#endif
---
+++ Hardware/Servo.c
@@ -0,0 +1,16 @@
+#include "stm32f10x.h"                  // Device header
+#include "PWM.h"
+
+void Servo_Init(void)
+{
+	PWM_Init();	
+}
+
+// 0      500
+// 180    2500
+
+void Servo_SetAngle(float Angle)
+{
+	PWM_SetCompare4(Angle/180*2000+500);
+}
+
